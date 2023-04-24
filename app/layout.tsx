ccc3df2ba0fe2f4442d8a4ad1b15613ccec080c4
MaxYu <39153424+yuyingyingmax@users.noreply.github.com>
2023-04-24 12:09:25 +0800

    name

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "Smart ChatChat",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Smart ChatChat",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
