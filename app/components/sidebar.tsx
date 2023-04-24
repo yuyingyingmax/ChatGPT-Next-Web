6df1258dcf1ed84c4e4640e23a5aec971307f9a2
MaxYu <39153424+yuyingyingmax@users.noreply.github.com>
2023-04-24 10:56:07 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -90,9 +90,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatChat</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          基于原生ChatGPT内核，您的私人AI助手
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
