4e9ccd9f0716715e16496a27d1fe120e9b6bd074
MaxYu <39153424+yuyingyingmax@users.noreply.github.com>
2023-04-24 10:39:21 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -50,27 +50,7 @@ const ENABLE_GPT4 = true;
 
 export const ALL_MODELS = [
   {
-    name: "gpt-4",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-0314",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-32k",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-32k-0314",
-    available: ENABLE_GPT4,
-  },
-  {
     name: "gpt-3.5-turbo",
-    available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-0301",
     available: true,
   },
 ] as const;
